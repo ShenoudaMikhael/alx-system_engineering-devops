9abf70863bf1a1240eda77d8961440682c37457a
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-28 00:08:43 +0200

    8. Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/from:(\+?\d+).*to:(\+?\d+).*flags:(-?\d:-?\d:-?\d:-?\d:-?\d)/).join(',')
+puts ARGV[0].scan(/from:(\+?\d+|[A-Za-z]*).*to:(\+?\d+)].*flags:(-?\d:-?\d:-?\d:-?\d:-?\d)/).join(',')
