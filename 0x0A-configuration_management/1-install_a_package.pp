7e9d37f7c7421c362c07dbeb3ec467297bbee143
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-19 19:48:38 +0200

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,4 @@
 # 1. Install flask 2.1.0 package 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python-pip']
+exec { 'pip3 install flask==2.1.0':
+  path    => ['/usr/bin', '/usr/sbin',];
 }
