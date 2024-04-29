7c6bf7ce9cbcfefe7f02f772ba3fb860e99447a1
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-30 02:01:32 +0300

    2. Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -10,53 +10,16 @@ class nginx_setup {
     before => File['/etc/nginx/sites-available/default'],
   }
 
-  file { ['/var/www/html', '/var/www/error']:
-    ensure => directory,
-    mode   => '0755',
+  file_line { 'http_header':
+    path  => '/etc/nginx/nginx.conf',
+    match => 'http {',
+    line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+  }
+  exec {'run2':
+    command => '/usr/sbin/service nginx start',
   }
 
-  file { '/var/www/html/index.html':
-    ensure  => present,
-    content => 'Hello World!',
-  }
 
-  file { '/var/www/html/404.html':
-    ensure  => present,
-    content => "Ceci n'est pas une page",
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    ensure  => present,
-    content => "
-      server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        root /var/www/html;
-        index index.html index.htm index.nginx-debian.html;
-        server_name _;
-        add_header X-Served-By \\$hostname;
-        location / {
-          try_files \\$uri \\$uri/ =404;
-        }
-        if (\\$request_filename ~ redirect_me){
-        rewrite ^ https://thedreamcatcher.dev permanent;
-        }
-        error_page 404 /404.html;
-        
-        
-        location = /404.html {
-          internal;
-        }
-      }
-    ",
-    notify  => Service['nginx'],
-  }
-
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    subscribe => File['/etc/nginx/sites-available/default'],
-  }
 
 }
 
