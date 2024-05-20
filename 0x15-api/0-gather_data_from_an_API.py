f48590e1d90b86d42913c561c7e9ba7a4c6b3859
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-20 23:30:10 +0300

    0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,21 +5,27 @@ from sys import argv
 import urllib.request
 import urllib.response
 
-with urllib.request.urlopen(
-    "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
-                            ) as resp:
-    user = json.loads(resp.read())
 
-with urllib.request.urlopen("https://jsonplaceholder.typicode.com/todos/"
-                            ) as response:
-    data = json.loads(response.read())
-    q = [i for i in data if i["userId"] == int(argv[1])]
-    completed = [i for i in q if i["completed"] is True]
-    print(
-        "Employee {} is done with tasks({}/{}):".format(
-            user["name"],
-            len(completed), len(q)
+def get():
+    with urllib.request.urlopen(
+        "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+                                ) as resp:
+        user = json.loads(resp.read())
+
+    with urllib.request.urlopen("https://jsonplaceholder.typicode.com/todos/"
+                                ) as response:
+        data = json.loads(response.read())
+        q = [i for i in data if i["userId"] == int(argv[1])]
+        completed = [i for i in q if i["completed"] is True]
+        print(
+            "Employee {} is done with tasks({}/{}):".format(
+                user["name"],
+                len(completed), len(q)
+            )
         )
-    )
-    for i in completed:
-        print("\t {}".format(i["title"]))
+        for i in completed:
+            print("\t {}".format(i["title"]))
+
+
+if __name__ == "__main__":
+    get()
