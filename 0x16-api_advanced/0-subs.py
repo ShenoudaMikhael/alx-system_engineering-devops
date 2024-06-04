f1870854701a1c6584f9e8b35c4ef8a9bea1f4b9
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-06-05 00:46:10 +0300

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,14 +5,18 @@ import requests
 
 def number_of_subscribers(subreddit):
     """numbeer of subs"""
-    headers = {"User-Agent": "user_agent"}
-    with requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        timeout=30,
-        headers=headers,
-    ) as r:
-        if r.status_code == 200:
+    try:
+        headers = {"User-Agent": "user_agent"}
+        with requests.get(
+            "https://www.reddit.com/r/{}/about.json".format(subreddit),
+            timeout=30,
+            headers=headers,
+            allow_redirects=False,
+        ) as r:
+            if r.status_code == 200:
 
-            return r.json()["data"]["subscribers"]
-        else:
-            return 0
+                return r.json()["data"]["subscribers"]
+            else:
+                return 0
+    except requests.RequestException:
+        return 0
