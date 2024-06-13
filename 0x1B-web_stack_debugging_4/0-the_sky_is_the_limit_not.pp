a4567f02deb728d8c049e0aeaf241c30a72b8556
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-06-13 22:29:12 +0300

    0. Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #change request limit
 exec { 'Change limit':
-  command  => 'sudo echo -e "* hard nofile 2500\n* soft nofile 25000" > /etc/security/limits.conf',
-  provider => shell,
+    command  => 'echo "ULIMIT=\"-n 25000\"" > /etc/default/nginx && sudo service nginx restart',
+    provider => shell,
 }
